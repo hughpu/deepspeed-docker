16e838be67695d5e8227578c36ac0bd328115e0a
purk <hughpu@hotmail.com>
2023-08-01 16:48:17 +0800

    fix: container sshd can not pass publickey authentication

--- start_deepspeed_container.sh
+++ start_deepspeed_container.sh
@@ -11,7 +11,7 @@ echo "Starting docker container: $name with image: $image"
 sudo docker run -itd --name $name \
     --net=host --shm-size="1gb" \
     --mount type=bind,source=$local_data_path,target=$container_data_path \
-    --mount type=bind,source=$local_ssh_path,target=$container_ssh_path \
     --gpus all $image
-sudo docker exec $name bash -c "chmod -R 777 $container_ssh_path"
-sudo docker exec $name bash -c "chmod -R 777 $container_data_path"
+sudo docker cp $local_ssh_path $name:$container_ssh_path
+sudo docker exec $name bash -c "sudo chown -R deepspeed:deepspeed $container_ssh_path"
+sudo docker exec $name bash -c "sudo chmod -R 777 $container_data_path"
